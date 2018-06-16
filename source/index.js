2e70bc9aef8a26174507e767e0b7e5096656bca3
Matheus Moura <matheus.moura13@gmail.com>
2018-06-15 21:57:25 -0300

    Adiciona funções basicas

--- source/index.js
+++ source/index.js
@@ -1,1 +1,13 @@
 console.log('teste');
+
+debugHelloWorld = ()=> {
+  console.log('Olá mundo');
+}
+
+debugSimpleText = ()=> {
+  console.log(asdk);
+}
+
+sumEqualFive = (num1, num2) => {
+  return num1 + num2;
+}
