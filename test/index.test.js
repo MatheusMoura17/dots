d1d550d0c0c4eeed6868b2b76905991a63655674
Matheus Moura <matheus.moura13@gmail.com>
2018-06-17 14:03:42 -0300

    Adiciona teste inicial do script Main

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,11 @@
 import assert from 'assert';
+import Main from '../source/index';
 
-describe('inicio', function () {
+describe('Main', function () {
 
-  it('2 + 2 deve ser igual a 4', function () {
-    assert.equal(4, 2 + 2);
+  it('2 + 3 deve ser igual a 5', function () {
+    this.main = new Main();
+    assert.equal(5, this.main.sumEqualFive(2,3));
   });
 
 }); 
