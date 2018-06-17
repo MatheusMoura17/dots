7e1525e211b8ed25cb15d0bec28788b79a7f83dc
Matheus Moura <matheus.moura13@gmail.com>
2018-06-17 13:57:41 -0300

    Configura script de teste

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 import assert from 'assert';
- 
-describe('inicio', function(){
 
-    it('2 + 2 deve ser igual a 4', function(){
-        assert.equal(4, 2 + 2);
-    });
+describe('inicio', function () {
+
+  it('2 + 2 deve ser igual a 4', function () {
+    assert.equal(4, 2 + 2);
+  });
 
 }); 
